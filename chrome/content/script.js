ebe9b9e39d57dab96ddea841987fa8384de6260c
Jan Sorondil Slaný <slanyj@centrum.cz>
2016-05-13 00:44:06 +0200

    Add options panel

--- chrome/content/script.js
+++ chrome/content/script.js
@@ -1,3 +1,34 @@
-window.onclick = function() {
-	document.getElementById("folderTree").invalidate();
+window.addEventListener("load", function(e) { 
+	document.getElementById("folderTree").classList.add("hello");
+	document.getElementById("my-panel").label = "Hello";
+	customDisplay("hello");
+}, false);
+
+var UnreadHighlighter = {
+	prefs: null,
+	
+	init = function() {
+		this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+			.getService(Components.interfaces.nsIPrefService)
+			.getBranch("extensions.m4_addon.");
+		this.prefs.addObserver("", this, false);
+	},
+	
+	applyOptions = function() {
+		
+	},
+	
+	observe: function(subject, topic, data) {
+		// Ignore everything which is not change of preferences
+		if (topic != "nsPref:changed") {
+			return;
+		}
+
+		switch(data) {
+			case "symbol":
+		/*		this.tickerSymbol = this.prefs.getCharPref("symbol").toUpperCase();
+				this.refreshInformation();*/
+				break;
+		}
+	},
 }
