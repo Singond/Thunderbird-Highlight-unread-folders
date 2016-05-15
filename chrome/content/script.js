9f6b21f77be4debc13bf5f5428aa360541f902b8
Jan Sorondil Slaný <slanyj@centrum.cz>
2016-05-15 12:00:15 +0200

    View updates automatically and immediately.

--- chrome/content/script.js
+++ chrome/content/script.js
@@ -8,11 +8,10 @@ function customDisplay(string) {
 
 /*window.setInterval(
 	function() {
-		//UnreadHighlighter.increment();
-		var text = UnreadHighlighter.prefs.getCharPref("unread-folder-style");
-		text = text + UnreadHighlighter.prefs.getCharPref("unread-account-style");
-//		customDisplay(text);
-	}, 1000); //update every second*/
+		// This works
+		let nameColumn = UnreadHighlighter.pane.columns.getColumnAt(0);
+		UnreadHighlighter.pane.treeBoxObject.invalidate(nameColumn);
+	}, 250); //update period*/
 
 var UnreadHighlighter = {
 	
@@ -29,13 +28,73 @@ var UnreadHighlighter = {
 			.getBranch("extensions.m4_addon.");
 		// Make it update the preferences on the fly by listening to their changes.
 		this.prefs.addObserver("", this, false);
-		/* Apply the preferences when starting, otherwise
-		 * they won't show up until they are changed for the first time. */
+		/* Apply the preferences once when starting TB, otherwise
+		 * they won't show up until they are changed. */
 		this.applyPreferenceAsClass("folder-highlight", this.pane);
 		this.applyPreferenceAsClass("account-highlight", this.pane);
+		
+		/* 
+		 * Now create a Mutation Observer to listen for changes to DOM
+		 * (like when the class of an element is updated).
+		 */
+/*		var observer = new MutationObserver (this.redraw);
+		var config = {subtree: true, attributes: true, characterData: true, chhildList: true};
+		// Register it with the folder pane
+		observer.observe(this.pane, config);*/
+		
+		this.setupListener();
 	},
 	
-	/** Class names associated with each option */
+	/* AUTOMATIC UPDATING (REDRAWING) */
+	
+	/**
+	 * Creates a new listener for folder changes
+	 * and registers it.
+	 */
+	setupListener: function() {
+		customDisplay("Initializing listener...");
+		var folderListener = {
+		/*	OnItemPropertyChanged: function(item, property, oldValue, newValue) {
+				customDisplay: ("heard");
+			}*/
+//			OnItemAdded: this.redraw,
+//			OnItemRemoved: this.redraw,
+//			OnItemPropertyChanged: this.redraw,
+			OnItemIntPropertyChanged: this.redraw,
+//			OnItemBoolPropertyChanged: this.redraw,
+//			OnItemUnicharPropertyChanged: this.redraw,
+//			OnItemPropertyFlagChanged: this.redraw,
+//			OnItemEvent: this.redraw,
+//			OnFolderLoaded: this.redraw,
+//			OnDeleteOrMoveMessagesCompleted: this.redraw
+		};
+		var flags = Components.interfaces.nsIFolderListener.intPropertyChanged;
+		MailServices.mailSession.AddFolderListener(folderListener, flags);
+	},
+	
+	i: 0,
+	/** The callback function to be run by observer. */
+	redraw: function() {
+		let fpane = UnreadHighlighter.pane;
+		let nameColumn = fpane.columns.getColumnAt(0);
+		fpane.treeBoxObject.invalidate(nameColumn);
+		// TODO Delete testing code:
+		UnreadHighlighter.i++;
+		customDisplay("redraw" + UnreadHighlighter.i);
+	},
+	
+	/*
+	 * OPTIONS HANDLING
+	 * I don't know how to conditionally load CSS,
+	 * so instead I implement the conditions as CSS classes
+	 * on the folderTree element.
+	 */
+	
+	/**
+	 * Class names associated with each option.
+	 * This object is used to translate the option name
+	 * to the CSS class name to mark the folderTree with.
+	 */
 	classNames: {
 		"folder-highlight": "highlight-folders",
 		"account-highlight": "highlight-accounts"
